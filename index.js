7f3642158bafa1b4237e5dd41845e0594859e210
elnata_correa@hormail.com <elnata_correa@hotmai.com>
2023-08-29 11:04:42 -0400

    adicionei mais coisas no index.js

--- index.js
+++ index.js
@@ -1,33 +1,78 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json()); // post
-
-// insominia ou postman
-
+// Habilitamos o processamento de JSON
+app.use(express.json());
 
 // Endpoint Principal
 app.get("/", function (req, res) {
-  res.json("Hello World");
+  res.send("Hello World");
 });
 
 // Endpoint /oi
 app.get("/oi", function (req, res) {
-  res.json("Olá, mundo!");
+  res.send("Olá, mundo!");
 });
 
-//endpoints de herois
-const lista = ["Mulher maravilha" , "Capitã Marvel" , "Homem de Ferro"];
-//                0                       1                  2
+// Endpoints de Herois
+const lista = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+//             0                    1                2
 
-//read all -> [GET] /herois
-app.get("/herois", function(req, res) {
-    res.json(lista);
+// Read All -> [GET] /herois
+app.get("/herois", function (req, res) {
+  res.send(lista);
 });
 
-//  get / put / post / patch / delete
+// Create -> [POST] /herois
+app.post("/herois", function (req, res) {
+  // console.log(req.body, typeof req.body);
 
+  // Extrai o nome do Body da Request (Corpo da Requisição)
+  const item = req.body.nome;
 
-app.listen(3000,()=>{
-  console.log("server rodando na porta 3000")
+  // Inserir o item na lista
+  lista.push(item);
+
+  // Enviamos uma resposta de sucesso
+  res.send("Item criado com sucesso!");
 });
+
+// Read By Id -> [GET] /herois/:id
+app.get("/herois/:id", function (req, res) {
+  // Pegamos o parâmetro de rota ID
+  const id = req.params.id - 1;
+
+  // Pegamos a informação da lista
+  const item = lista[id];
+
+  // Exibimos o item na resposta do endpoint
+  res.send(item);
+});
+
+// Update -> [PUT] /herois/:id
+app.put("/herois/:id", function (req, res) {
+  // Pegamos o parâmetro de rota ID
+  const id = req.params.id - 1;
+
+  // Extrai o nome do Body da Request (Corpo da Requisição)
+  const item = req.body.nome;
+
+  // Atualizamos a informação na lista de registros
+  lista[id] = item;
+
+  res.send("Item editado com sucesso!");
+});
+
+//Delete -> [DELETE] /herois/:id
+app.delete("herois/:id", function(req, res) {
+  //Pegamos o parâmetro de rota ID
+  const id = req.params.id - 1;
+
+  //excluir o item da lista
+  delete lista[id];
+
+  res.send("Item excluido com sucesso");
+
+});
+
+app.listen(3000);
